32e01ad7fb866e8aeeb843a596b1486b9897ba74
powerdreams-1 <95529902+powerdreams-1@users.noreply.github.com>
2021-12-10 14:39:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express =require ("express");
 const app =express ( );
+const morkuserData=[
+{name:'martin'},
+{name:'musyoka'}
+]
+app.get('/users',function (req,res){
+res.json({
+success:true ;
+message:successfully got users.Nice!',
+users:MorkUserData
+})
 
 app.listen (8000,function ( ){
-console.log("server is running")
+console.log("server is listening")
 })
