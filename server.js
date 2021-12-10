5f93a539694475aba9054a2f698a84f28e7c12b4
powerdreams-1 <95529902+powerdreams-1@users.noreply.github.com>
2021-12-10 14:49:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ success:true ;
 message:successfully got users.Nice!',
 users:MorkUserData
 })
-
+app.get('/users/:id',function (req,res){
+console.log (req.params.id)
+res.json ({
+succsess :true,
+message :'i got one user',
+user : req.params.id
+})
 app.listen (8000,function ( ){
 console.log("server is listening")
 })
